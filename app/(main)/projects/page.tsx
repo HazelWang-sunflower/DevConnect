1b906a8550f88b25248e911f7c2400bf6232dc09
Hazel <wyh_sunflower@163.com>
2025-01-16 14:06:27 +0800

    fix issue

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -11,7 +11,7 @@ export default async function Projects() {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    redirect("/auth/signin");
+    redirect("/login");
   }
   const projects = await prisma.project.findMany({
     where: {
